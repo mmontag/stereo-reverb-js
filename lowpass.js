57ba9dfa0dcbe2eaea84c800e02dd755190b0d6a
Matt Montag <montag@spotify.com>
2018-07-08 19:32:47 -0700

    Initial project structure

---
+++ lowpass.js
@@ -0,0 +1,23 @@
+/*
+A Single-Pole Low Pass Filter
+Formula for coefficients taken from R. Boulanger, 2011, p.486
+Filter acts on a one sample delay affected by pole coefficients
+*/
+export default class Lowpass {
+  constructor(sampleRate, cutoffFreq) {
+    this.prev = 0;
+    this.sampleRate = sampleRate;
+    this.setCutoffFreq(cutoffFreq);
+  }
+
+  setCutoffFreq(cutoffFreq) {
+    this.cutoffFreq = cutoffFreq;
+    const costh = 2 - Math.cos(2 * Math.PI * this.cutoffFreq / this.sampleRate);
+    this.coef = Math.sqrt(costh * costh - 1) - costh;
+  }
+
+  render(input) {
+    this.prev = input * (1 + this.coef) - (this.prev * this.coef);
+    return this.prev;
+  }
+}
